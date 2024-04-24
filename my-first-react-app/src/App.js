5c6794574fa403751ce8ce34a75a533be81d78b0
soraqah <soraqah@gmailcom>
2024-04-25 00:52:43 +0300

    this just my first day's work

---
+++ my-first-react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './commponat/Navbar';
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+            { "Other components and content" }
+        </div>
+    );
+}
+
+export default App;
+
---
+++ my-first-react-app/src/commponat/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+         <nav>
+            <ul>
+                <li><a href="www.google.com">Home</a></li>
+                <li><a href="www.youtube.com">About</a></li>
+                <li><a href="www.amzon.com">Services</a></li>
+                <li><a href="www.facebook.com">Contact</a></li>
+            </ul>
+        </nav>
+        </div>
+    )
+}
+
+export default Navbar
